d9419c7367aff97948521a07771bd862c25e539c
Jim Doan <kazma131100@gmail.com>
2024-08-12 17:26:14 -0400

    Lesson 2 Complete

--- nordic_fundamental_course/lesson_2/exercise_2/src/main.c
+++ nordic_fundamental_course/lesson_2/exercise_2/src/main.c
@@ -10,7 +10,7 @@
 #include <zephyr/drivers/gpio.h>
 
 /* STEP 9 - Increase the sleep time from 100ms to 10 minutes  */
-#define SLEEP_TIME_MS   100
+#define SLEEP_TIME_MS   100*60*1000
 
 /* SW0_NODE is the devicetree node identifier for the "sw0" alias */
 #define SW0_NODE	DT_ALIAS(sw0) 
@@ -21,10 +21,27 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 
-
 /* STEP 4 - Define the callback function */
+void button_pressed(const struct device *dev, struct gpio_callback *cd, uint32_t pins){
+	//QUES: What is a callback function? 
+	//ANS:  This function is called whenever interrupt is activated
+	//QUES: why do I passed all 3 different paramenter to this function?
+	//		Can I just pass the structure, and not the rest?
+	//ANS:  ************
+	gpio_pin_toggle_dt(&led);
+	//Passing in address of the struct led
+}
 
 /* STEP 5 - Define a variable of type static struct gpio_callback */
+//QUES: Why do we need this struct? 
+//		What is button_cb_data?
+//ANS:  This statement is just a define statement.
+//		This is to allocate the right budget for the memeory.
+//		No value is added in this struct. Just an empty sturct.
+//		Idk why can't we just define everything up here instead of down there.
+static struct gpio_callback button_cb_data;
+
+
 
 int main(void)
 {
@@ -47,18 +64,33 @@ int main(void)
 	if (ret < 0) {
 		return -1;
 	}
-	/* STEP 3 - Configure the interrupt on the button's pin */
 
+	/* STEP 3 - Configure the interrupt on the button's pin */
+	ret =  gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+
+	
 	/* STEP 6 - Initialize the static struct gpio_callback variable   */
+	//QUES: What is gpio_init_callback function?
+	//		What parameters does this function received?
+	//ANS:	The parameters are:
+	//			- Address of the previously define gpio_callback struct
+	//			- The  previously callback function (what the MC should do when interrupt is detected) 
+	//			- The BIT function, use to create bit mask for the value of: button.pin
+	//			  ex: if button.pin = 10, then the result is 1 << 10 is 1024
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+
 
 	/* STEP 7 - Add the callback function by calling gpio_add_callback()   */
+	//The parameters are: 
+	//	- Port element in button struct
+	//	- Address of the callback struct 
+	gpio_add_callback(button.port, &button_cb_data);
 
 	while (1) {
-		/* STEP 8 - Remove the polling code */
-        bool val = gpio_pin_get_dt(&button);
-        gpio_pin_set_dt(&led,val);		
-     
+		/* STEP 8 - Remove the polling code */	
+		//Sleep time can be as long as 10 minutes and device would work just fine.
         k_msleep(SLEEP_TIME_MS); 
+
 	}
         
 }
