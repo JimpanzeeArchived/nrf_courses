4a2816a10b72ca31a0e6d6c3406afc304fe55335
Jim Doan <kazma131100@gmail.com>
2024-09-15 16:26:50 -0400

    lesson 4 exercise 3 and lesson 5 exercise 1

---
+++ nordic_fundamental_course/lesson_5/fund_less5_exer1/build_1/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf5340dk_nrf5340_cpuapp_ns" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "C:/ncs/nordic_courses/nordic_fundamental_course/lesson_5/fund_less5_exer1/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
